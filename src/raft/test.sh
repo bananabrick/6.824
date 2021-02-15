0ea4ae0c41b33731c25c70d40b1ab50104fa21f5
bananabrick <a26nair@edu.uwaterloo.ca>
2021-02-14 22:16:00 -0500

    remove weird comments from stackoverflow bash script

--- src/raft/test.sh
+++ src/raft/test.sh
@@ -2,10 +2,7 @@
 
 for i in {1..20}
 do
-	time go test -race -run 2A & # Put a function in the background
+	time go test -race -run 2A &
 done
- 
-## Put all cust_func in the background and bash 
-## would wait until those are completed 
-## before displaying all done message
+
 wait

5302f31767fc00ef4ed5ec787689c7347950bf8a
bananabrick <a26nair@edu.uwaterloo.ca>
2021-05-08 12:34:59 -0400

    run linter and stuff

--- src/kvraft/common.go
+++ src/kvraft/common.go
@@ -1,71 +1,73 @@
 package kvraft
 
 import (
-    "log"
+	"log"
 )
 
 // Err associated with RPCs to the
 // kvservers.
 type Err string
+
 const (
-    // OK resp
-    OK Err            = "OK"
-    // ErrNoKey signifies a key not found response
-    ErrNoKey Err       = "ErrNoKey"
-    // ErrWrongLeader signifies that we contacted the wrong leader
-    ErrWrongLeader Err = "ErrWrongLeader"
+	// OK resp
+	OK Err = "OK"
+	// ErrNoKey signifies a key not found response
+	ErrNoKey Err = "ErrNoKey"
+	// ErrWrongLeader signifies that we contacted the wrong leader
+	ErrWrongLeader Err = "ErrWrongLeader"
 )
 
 type clientOp string
+
 const (
-    putOp clientOp = "Put" // update or set value associated with a key
-    appendOp clientOp = "Append" // Append value to the key string
+	putOp    clientOp = "Put"    // update or set value associated with a key
+	appendOp clientOp = "Append" // Append value to the key string
 )
 
 const debug = 0
 
 func dprintln(a ...interface{}) (n int, err error) {
-       if debug > 0 {
-               log.Println(a...)
-       }
-       return
+	if debug > 0 {
+		log.Println(a...)
+	}
+	return
 }
 
 // PutAppendArgs does a Put RPC or an Append RPC
 type PutAppendArgs struct {
-    Key   string
-    Value string
-    Op    clientOp
-    ClientID int64
-    SequenceNum int
-    // Got back reply for SeqNum <= [SeenSeqUntil]
-    SeenSeqUntil int
+	Key         string
+	Value       string
+	Op          clientOp
+	ClientID    int64
+	SequenceNum int
+	// Got back reply for SeqNum <= [SeenSeqUntil]
+	SeenSeqUntil int
 }
 
 // PutAppendReply is the reply to the PutAppendArgs RPC
 type PutAppendReply struct {
-    Err Err
+	Err Err
 }
 
 // GetArgs RPC
 type GetArgs struct {
-    Key string
-    ClientID int64
-    SequenceNum int
-    
-    // Got back reply for SeqNum <= [SeenSeqUntil]
-    SeenSeqUntil int
+	Key         string
+	ClientID    int64
+	SequenceNum int
+
+	// Got back reply for SeqNum <= [SeenSeqUntil]
+	SeenSeqUntil int
 }
 
 // GetReply is the reply to the GetArgs RPC
 type GetReply struct {
-    Err   Err
-    Value string
+	Err   Err
+	Value string
 }
 
 func max(a, b int) int {
-    if a >= b {
-        return a
-    }
-    return b
+	if a >= b {
+		return a
+	}
+	return b
 }
